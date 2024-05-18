4ebcab35a123cc827292bb1bd1eaa12312c05e13
veronicaridolfi <veronica.ridolfi@studenti.isiaurbino.net>
2024-05-18 21:04:55 +0200

    Create oscillator.js

---
+++ presentazione/assets/sketches/sega/oscillator.js
@@ -0,0 +1,48 @@
+
+let osc, playing, freq, amp;
+
+
+function setup() {
+  let cnv = createCanvas(100,100);
+  cnv.mousePressed(playOscillator);
+
+  osc = new p5.Oscillator('sawtooth')
+}
+
+function draw() {
+  background(255)
+  
+  let mx = 10;
+	let my = 2;
+  
+	//sinusoide
+  let numero_di_punti = 2000;
+  noStroke();
+  
+  freq = 200
+  amp  = 2
+
+triangle(20, 80, 80, 20, 80, 80)
+
+
+  fill(255,0,0)
+  text('tap to play', 20, 15);
+ 
+
+  if (playing) {
+    // smooth the transitions by 0.1 seconds
+    osc.freq(freq, 0.1);
+    osc.amp(amp, 0.1);
+  }
+}
+
+function playOscillator() {
+  osc.start()
+  playing = true
+}
+function mouseReleased() {
+  // ramp amplitude to 0 over 0.5 seconds
+  osc.amp(0, 0.5);
+  playing = false;
+}
+
